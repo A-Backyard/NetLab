99497df792986d9db3f72c7689c533244a441848
A-Backyard <anton.backyard@gmail.com>
2019-12-17 22:31:49 +0200

    Initial commit

---
+++ psd to html (church adapt)/church html/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+
+/* $('.slider__inner').slick({
+  dots: true,
+  arrows: false,
+  speed: 500,
+  cssEase: 'linear'
+})
+
+$('.submain__slider').slick({
+  dots: true,
+  arrows: true,
+  speed: 500,
+  cssEase: 'linear',
+  infinite: false
+});
+*/
+$('.header__btn').on('click', function(){
+  $('.menu ul').slideToggle();
+});
+});
